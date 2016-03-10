d8756bd6983e925ed97be302855ef349c3fff3c6
eric-greencomb <1121453646@qq.com>
2016-03-10 19:12:45 +0800

    change tmpl

--- handler/index.go
+++ handler/index.go
@@ -7,8 +7,8 @@ import (
 
 func ShowIndex(ctx *middleware.Context, ren render.Render) {
 	ctx.Set("Website", "Banerwai")
-	ctx.Set("WebsiteTitle", "A Freelancer Website")
-	ctx.Set("WebsiteDetail", "This is a website for freelancer.")
+	ctx.Set("WebsiteTitle", "A Banerwai Website")
+	ctx.Set("WebsiteDetail", "This is a website for banerwai.")
 
 	ren.HTML(200, "index/index", ctx, render.HTMLOptions{
 		Layout: "layout/layout_index",
